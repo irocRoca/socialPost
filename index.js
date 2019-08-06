52fe62c4ef2b3cc6f1a5c0523d61be89c3394c53
Carlos Roca <cr5394@hotmail.com>
2019-08-05 23:28:05 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => "Hello"
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen({ port: 5000 }).then(({ url }) => console.log(url));
