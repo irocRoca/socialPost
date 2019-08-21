2854757bb71014f590c2362d209ba053888faed2
Carlos Roca <cr5394@hotmail.com>
2019-08-20 23:50:10 -0400

    Test 2

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const server = new ApolloServer({
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/build/index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 }
 
