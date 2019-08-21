f2b3179fc45181d16831bd979abaeb62b1219419
Carlos Roca <cr5394@hotmail.com>
2019-08-20 23:39:16 -0400

    Test

---
+++ build/precache-manifest.540923e75c116b19839fc3babb4742d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3636c3f7e2dd5625d07249c8608df70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f1e7fcd7f49417843f",
+    "url": "/static/css/main.8e43f6a1.chunk.css"
+  },
+  {
+    "revision": "8199daafafba737141b9",
+    "url": "/static/js/2.5079224f.chunk.js"
+  },
+  {
+    "revision": "d7f1e7fcd7f49417843f",
+    "url": "/static/js/main.dec5514f.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
